e0928a4b75033e144d729979df8b9280bf432d68
Thybow <tmilville.pro@gmail.com>
2023-09-04 19:21:00 +0200

    debug + http solution

--- js/utils/weatherStackAPI.js
+++ js/utils/weatherStackAPI.js
@@ -1,11 +1,11 @@
-const ACCESS_KEY = ''
+const ACCESS_KEY = 'f71ba1ecf0a2fe236a840990b40069a4'
 
 const _retrieveWeatherForecastMockedData = () => fetch('/data/weather-api-mocked-data.json')
 .then(res => res.json())
 .catch(err => console.log("Oh no", err))
 
 
-const _retrieveWeatherForecastApiData = coordinates => fetch(`https://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
+const _retrieveWeatherForecastApiData = coordinates => fetch(`http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${coordinates.lat},${coordinates.lng}`)
     .then(res => res.json())
     .catch(err => console.log("Oh no", err))
 
